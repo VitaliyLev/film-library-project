11277b6c68f22f088f919b9c6fa4e424b487d7bd
VoloshynVolodymyr <mailvoloshyn@gmail.com>
2022-11-26 18:17:21 +0200

    pagination(1)

--- src/scripts/get_api/get_trendApi.js
+++ src/scripts/get_api/get_trendApi.js
@@ -5,21 +5,22 @@ const URL = 'https://api.themoviedb.org/3/';
 const MY_MOVIE_KEY = '388e7c1d810433186d944819803a330c';
 const END_POINT_TREND_GALLERY = 'trending/movie/day?';
 
-class ApiImagesTrendRequest {
+export class ApiImagesTrendRequest {
   constructor() {
     this.url = URL;
     this.key = MY_MOVIE_KEY;
   }
 
-  getOptionsMain() {
+  getOptionsMain(page=1) {
     const options = new URLSearchParams({
       api_key: `${this.key}`,
+      page,
     });
     return options;
   }
 
-  async getImagesTrendGallery() {
-    const option = this.getOptionsMain();
+  async getImagesTrendGallery(page) {
+    const option = this.getOptionsMain(page);
     const response = await axios.get(
       `${URL}${END_POINT_TREND_GALLERY}${option}`
     );
@@ -34,13 +35,14 @@ class ApiImagesTrendRequest {
 
 const responseTrendApiImg = new ApiImagesTrendRequest();
 
-function renderTrendGallery() {
+export function renderTrendGallery(page) {
     responseTrendApiImg
-    .getImagesTrendGallery()
-    .then(data => renderHtmlMurkup(data))
+    .getImagesTrendGallery(page)
+    .then(data => {renderHtmlMurkup(data);
+      console.log(data.total_pages, data.total_results);
+    })
     // .then(data => console.log(data))
     
     .catch(err => err.message);
 }
-
 renderTrendGallery();
