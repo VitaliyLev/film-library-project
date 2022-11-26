7d68902dae9d212911d32875b1a5eb69ed331dd7
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-11-26 23:06:20 +0200

    add info film modal show

--- src/scripts/refs.js
+++ src/scripts/refs.js
@@ -2,4 +2,8 @@ export const refs = {
   //приклад
   galleryEl: document.querySelector('.gallery__list'),
   formElem: document.querySelector('.js-search-form'),
+  closeBtnModalEl: document.querySelector('.js-close-btn'),
+  modalEl: document.querySelector('.modal'),
+  wraperModalEl: document.querySelector('.modal-wrapper'),
+  filmDetailsEl: document.querySelector('.film-details'),
 };
