746beb7b22cf792b29ec0cf142621b92871c50ee
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-11-27 17:45:09 +0200

    fix modal

--- src/scripts/get_api/get_search_by_id.js
+++ src/scripts/get_api/get_search_by_id.js
@@ -38,9 +38,6 @@ class ApiImagesSearchByIdRequest {
   }
 }
 
-// let modal = document.querySelector('.modal');
-// let wraperModall = document.querySelector('.modal-wrapper');
-// let filmDetailsEl = document.querySelector('.film-details');
 const responseByIdApiImg = new ApiImagesSearchByIdRequest();
 
 function renderByIdGallery() {
@@ -65,8 +62,8 @@ function handleClickOnImgCard(e) {
   refs.wraperModalEl.classList.remove('modal-hidden');
   refs.modalEl.classList.remove('modal-hidden');
 
-  //
-  refs.filmDetailsEl.innerHTML = '';
+  refs.imageWrapperEl.innerHTML = '';
+  refs.filmDetailsWrapperEl.innerHTML = '';
   renderByIdGallery();
 }
 
@@ -87,64 +84,44 @@ function renderModalMarkup(data) {
 
   const genr = genres.map(genr => genr.name);
 
-  const markup = `
+  const markupImg = `
       <img class="film-details__image js-image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="#" />
-    </div>
-    <div class="film-details__wrapper">
-      <h2 class="film-details__title js-title">${title}</h2>
-      <ul class="film-details__list list">
-        <li class="film-details__item">
-          <p class="film-details__text lang-rating">Vote / Votes</p>
-          <span class="film-details__span film-details__span--accent js-vote"
-            >${vote_average.toFixed(1)}</span
-          >
-          <span>/</span>
-          <span
-            class="film-details__span film-details__span--noaccent js-vote-count"
-            >${vote_count}</span
-          >
-        </li>
-        <li class="film-details__item">
-          <p class="film-details__text lang-popularity">Popularity</p>
-          <span class="film-details__span js-popularity">${popularity.toFixed(
-            1
-          )}
-          </span>
-        </li>
-        <li class="film-details__item">
-          <p class="film-details__text lang-original">Original Title</p>
-          <span class="film-details__span js-title-orig"
-            >${original_title}</span
-          >
-        </li>
-        <li class="film-details__item">
-          <p class="film-details__text lang-genre">Genre</p>
-          <span class="film-details__span js-genres">${genr}</span>
-        </li>
-      </ul>
-      <p class="film-details__about lang-about">About</p>
-      <p class="film-details__overview js-about">
-        ${overview}
-      </p>
-      <ul class="film-details-btn__list js-detail-btn">
-        <li class="film-details-btn__item">
-          <button
-            class="film-details-btn lang-addwatched js-btn-modal-watched"
-            type="button"
-          >
-            add to Watched
-          </button>
-        </li>
-        <li class="film-details-btn__item">
-          <button
-            class="film-details-btn lang-addqueue js-btn-modal-queue"
-            type="button"
-          >
-            add to queue
-          </button>
-        </li>
-      </ul>
-    </div>`;
+      `;
+  const markDetails = `
+  <h2 class="film-details__title js-title">${title}</h2>
+  <ul class="film-details__list list">
+    <li class="film-details__item">
+      <p class="film-details__text lang-rating">Vote / Votes</p>
+      <span class="film-details__span film-details__span--accent js-vote"
+        >${vote_average.toFixed(1)}</span
+      >
+      <span>/</span>
+      <span
+        class="film-details__span film-details__span--noaccent js-vote-count"
+        >${vote_count}</span
+      >
+    </li>
+    <li class="film-details__item">
+      <p class="film-details__text lang-popularity">Popularity</p>
+      <span class="film-details__span js-popularity">${popularity.toFixed(1)}
+      </span>
+    </li>
+    <li class="film-details__item">
+      <p class="film-details__text lang-original">Original Title</p>
+      <span class="film-details__span js-title-orig"
+        >${original_title}</span
+      >
+    </li>
+    <li class="film-details__item">
+      <p class="film-details__text lang-genre">Genre</p>
+      <span class="film-details__span js-genres">${genr}</span>
+    </li>
+  </ul>
+  <p class="film-details__about lang-about">About</p>
+  <p class="film-details__overview js-about">
+    ${overview}
+  </p>`;
 
-  refs.filmDetailsEl.insertAdjacentHTML('beforeend', markup);
+  refs.imageWrapperEl.insertAdjacentHTML('beforeend', markupImg);
+  refs.filmDetailsWrapperEl.insertAdjacentHTML('beforeend', markDetails);
 }
