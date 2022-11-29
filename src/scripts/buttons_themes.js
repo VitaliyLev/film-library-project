a00e3a26295b4dc4f151d0a8d82ce875f67a8d24
VoloshynVolodymyr <mailvoloshyn@gmail.com>
2022-11-29 14:12:37 +0200

    themes(localStorage)

--- src/scripts/buttons_themes.js
+++ src/scripts/buttons_themes.js
@@ -1,22 +1,24 @@
 import { refs } from './refs';
 
-
 let light = true;
 
 refs.ligthBtn.addEventListener('click', setLightTheme);
 refs.darkBtn.addEventListener('click', setDarkTheme);
 
-function setDarkTheme(){
-    if (!light) return;
-    light = false;
-    refs.screen.classList.remove('light_theme');
-    refs.screen.classList.add('dark_theme');
-           }
-
-function setLightTheme(){
-    if (light) return;
-    light = true;
-    refs.screen.classList.remove('dark_theme');
-    refs.screen.classList.add('light_theme');
-    
+export function setDarkTheme() {
+  localStorage.setItem('theme', 'dark');
+  if (!light) return;
+  light = false;
+  refs.screen.classList.remove('light_theme');
+  refs.screen.classList.add('dark_theme');
 }
+
+export function setLightTheme() {
+  localStorage.setItem('theme', 'light');
+  if (light) return;
+  light = true;
+  refs.screen.classList.remove('dark_theme');
+  refs.screen.classList.add('light_theme');
+}
+
+if (localStorage.getItem('theme') === 'dark') setDarkTheme();
