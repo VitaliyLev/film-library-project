63248c206d87b5c9a2c74941d385b7b6d7fe4bc4
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-11-29 21:08:41 +0200

    add fix close modal

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -5,26 +5,9 @@ refs.closeBtnModalEl.addEventListener('click', function () {
   refs.modalEl.classList.add('modal-hidden');
 });
 
-function clearEventListeners() {
-    window.removeEventListener('click', closeModalByClick);
-    window.removeEventListener('keydown', closeModalByEsc);   
-    refs.closeBtnModalEl.removeEventListener('click', closeModal);
-
-    console.log('hello');
+window.onclick = function(event) {
+    if (event.target === refs.modalEl) {
+      refs.wraperModalEl.classList.add('modal-hidden');
+      refs.modalEl.classList.add('modal-hidden');
   }
-
-
- function closeModalByEsc(e) {
-    if (e.key === 'Escape') {
-     refs.modalEl.classList.add('modal-hidden');
-      clearEventListeners();
-    }
-  }
-
-  function closeModalByClick(e) {
-    if (e.target === refs.modalEl) {
-     refs.modalEl.classList.add('modal-hidden');
-      clearEventListeners();
-    }
-  }
-
+} 
